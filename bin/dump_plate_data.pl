93c80a8cd8645e8480eff59f94fba192f2c3fe30
Sajith Perera <sajith.perera@gmail.com>
2012-08-23 07:29:40 +0100

    modifyy dump_plate to produce output for new plate types

--- bin/dump_plate_data.pl
+++ bin/dump_plate_data.pl
@@ -10,12 +10,21 @@ use DateTime::Format::ISO8601;
 use Pod::Usage;
 
 const my %CLASS_FOR => (
-    design => 'LIMS2::HTGT::Migrate::PlateData::Design',
-    pcs    => 'LIMS2::HTGT::Migrate::PlateData::PCS',
-    vtp    => 'LIMS2::HTGT::Migrate::PlateData::VTP',
+    #design => 'LIMS2::HTGT::Migrate::PlateData::Design',
+    #pcs    => 'LIMS2::HTGT::Migrate::PlateData::PCS',
+    #vtp    => 'LIMS2::HTGT::Migrate::PlateData::VTP',
+    fep      => 'LIMS2::HTGT::Migrate::PlateData::FEP',
+    sep      => 'LIMS2::HTGT::Migrate::PlateData::SEP',
+    xep      => 'LIMS2::HTGT::Migrate::PlateData::XEP',
+    ep_pick  => 'LIMS2::HTGT::Migrate::PlateData::EPPICK',
+    sep_pick => 'LIMS2::HTGT::Migrate::PlateData::SEPPICK',
+    fp       => 'LIMS2::HTGT::Migrate::PlateData::FP',
+    sfp      => 'LIMS2::HTGT::Migrate::PlateData::SFP',
 );
 
 my $log_level = $WARN;
+my @plate_names;
+my $plate_name_regex;
 
 GetOptions(
     'debug'           => sub { $log_level => $DEBUG },
@@ -23,7 +32,8 @@ GetOptions(
     'type=s'          => \my $plate_type,
     'limit=i'         => \my $limit,
     'created-after=s' => \my $created_after,    
-    'plate_name=s'    => \my @plate_names,
+    'plate_name=s'    => \@plate_names,
+    'plate_regex=s'   => \$plate_name_regex,
 ) or pod2usage(2);
 
 pod2usage( "Plate type must be specified" )
@@ -46,7 +56,12 @@ Log::Log4perl->easy_init(
     }
 );
 
-my $worker = $class->new( limit => $limit, created_after => $created_after, plate_names => \@plate_names );
+my $worker = $class->new(
+    limit => $limit,
+    created_after => $created_after,
+    plate_names => \@plate_names,
+    plate_name_regex => $plate_name_regex,
+);
 
 $worker->dump_plate_data();
 
