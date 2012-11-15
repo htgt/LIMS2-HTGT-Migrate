169d342a6fe8654cd2b2e8c0f4c2d136f6b978fb
Sajith Perera <sajith.perera@gmail.com>
2012-11-15 10:01:16 +0000

    extra error checks, simplify design type check, code tidy up

--- bin/create_oligos_lims2.pl
+++ bin/create_oligos_lims2.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/env perl
-
 use strict;
+use warnings FATAL => 'all';
+
 use HTGT::DBFactory;
 use LIMS2::REST::Client;
 use Perl6::Slurp;
 use Const::Fast;
-use Smart::Comments;
 use List::MoreUtils qw(uniq);
 use Log::Log4perl qw( :easy );
 use Try::Tiny;
@@ -24,8 +24,9 @@ my $schema = HTGT::DBFactory->connect('eucomm_vector');
     );
 
     GetOptions(
-        'verbose'     => sub { $log4perl{level} = $INFO },
-    );
+        'verbose' => sub { $log4perl{level} = $INFO },
+        'debug'   => sub { $log4perl{level} = $DEBUG },
+    ) and @ARGV = 1;
 
     Log::Log4perl->easy_init( \%log4perl );
     $lims2 = LIMS2::REST::Client->new_with_config();
@@ -33,10 +34,14 @@ my $schema = HTGT::DBFactory->connect('eucomm_vector');
     my @designs = split( "\n", slurp($ARGV[0]) );
 
     for my $design_id (@designs){
-      migrate_design_oligo_loci( $design_id );
+        try{
+            migrate_design_oligo_loci( $design_id );
+        }
+        catch {
+            WARN( $_ );
+        };
     }
 }
-
 
 sub migrate_design_oligo_loci {
     my $design_id = shift;
@@ -44,7 +49,9 @@ sub migrate_design_oligo_loci {
     Log::Log4perl::NDC->push( $design_id );
 
     my $design = $schema->resultset('Design')->find({ 'design_id'=> $design_id});
-    my $oligos = oligos_for($design, type_for($design));
+    die("Invalid design id $design_id") unless $design;
+
+    my $oligos = oligos_for($design);
 
     INFO( "Create design oligo loci for $design_id on assembly $ASSEMBLY" );
 
@@ -53,14 +60,14 @@ sub migrate_design_oligo_loci {
     };
     die("design does not exist in lims2: " . $design_id) unless $lims2_design;
 
-    my %lims_oligos_details = map {$_->{type} => $_->{locus}{assembly}} @{$lims2_design->{oligos}};
-    for my $oligo (@{$oligos}){
-        if (! exists $lims_oligos_details{$oligo->{oligo_type}} or $lims_oligos_details{$oligo->{oligo_type}} ne $oligo->{assembly}){
-            ### CREATED: create oligo for design: $oligo->{design_id} . ' type ' . $oligo->{oligo_type}
+    my %lims_oligos_details = map { $_->{type} => $_->{locus}{assembly} } @{$lims2_design->{oligos}};
+    for my $oligo ( @{$oligos} ) {
+        if (! exists $lims_oligos_details{$oligo->{oligo_type}} or $lims_oligos_details{$oligo->{oligo_type}} ne $oligo->{assembly} ) {
+            INFO('CREATED: create oligo for design: ' . $oligo->{design_id} . ' type ' . $oligo->{oligo_type} );
             $lims2->POST( 'design_oligo_locus', $oligo );
         }
         else {
-            ### ALREADY EXISTS: did not create oligo for design: $oligo->{design_id} . ' type ' . $oligo->{oligo_type}
+            WARN('ALREADY EXISTS: did not create oligo for design: ' . $oligo->{design_id} . ' type ' . $oligo->{oligo_type} );
         }
     }
 }
@@ -80,15 +87,15 @@ sub retrieve_lims2_design {
 }
 
 sub oligos_for {
-    my ( $design, $design_type ) = @_;
+    my ( $design ) = @_;
 
-    my @oligoloci;
+    my @oligo_loci;
 
     my $features = $design->validated_display_features;
 
     for my $oligo_name ( @OLIGO_NAMES ) {
         my $oligo = $features->{$oligo_name} or next;
-        push @oligoloci, {
+        push @oligo_loci, {
             design_id  => $design->design_id,
             oligo_type => $oligo_name,
             assembly   => $ASSEMBLY,
@@ -98,13 +105,13 @@ sub oligos_for {
             chr_strand => $oligo->feature_strand
         };
     }
-    sanity_check_oligos( $design_type, \@oligoloci );
+    sanity_check_oligos( $design, \@oligo_loci );
 
-    return \@oligoloci;
+    return \@oligo_loci;
 }
 
 sub sanity_check_oligos {
-    my ( $design_type, $oligos ) = @_;
+    my ( $design, $oligos ) = @_;
     my %oligos_hash = map {$_->{oligo_type} => $_} @{$oligos};
 
     die "Design has no validated oligos with $ASSEMBLY locus\n"
@@ -118,7 +125,7 @@ sub sanity_check_oligos {
 
     my @oligo_names = $strands[0] eq 1 ? @OLIGO_NAMES : reverse @OLIGO_NAMES;
 
-    if ( $design_type eq 'insertion' or $design_type eq 'deletion' ) {
+    if ( $design->design_type && ( $design->design_type =~ /^Del/ || $design->design_type =~ /^Ins/ ) ) {
         @oligo_names = grep { $_ ne 'U3' and $_ ne 'D5' } @oligo_names;
     }
 
@@ -136,43 +143,4 @@ sub sanity_check_oligos {
             unless $oligos_hash{$o1}->{chr_end} <= $oligos_hash{$o2}->{chr_start};
     }
 }
-
-sub type_for {
-    my $design = shift;
-
-    my $intron_replacement =
-        $design->search_related_rs( 'design_user_comments',
-                                  {
-                                      'category.category_name' => 'Intron replacement'
-                              },
-                              {
-                                  join => 'category'
-                              }
-                          )->count;
-
-    if ( $intron_replacement > 0 ) {
-        return 'intron-replacement';
-    }
-
-    if ( $design->is_artificial_intron ) {
-        return 'artificial-intron';
-    }
-
-    my $dt = $design->design_type;
-
-    if ( !defined($dt) || $dt =~ /^KO/ ) {
-        return 'conditional';
-    }
-
-    if ( $dt =~ /^Ins/ ) {
-        return 'insertion';
-    }
-
-    if ( $dt =~ /^Del/ ) {
-        return 'deletion';
-    }
-
-    die "Unrecognized design type '$dt'\n";
-}
-
 
